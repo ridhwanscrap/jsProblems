223e743d6803efe8d87b428ab336ecd97d612237
Ridwan Alam <ridhwanalam11@gmail.com>
2025-03-26 15:17:31 +0600

    prblem4 solved

--- problem4.js
+++ problem4.js
@@ -4,3 +4,16 @@
 // isPalindrome("hello") should return false
 // isPalindrome("rotator") should return true
 // isPalindrome("peep") should return true
+
+function isPalindrome(val) {
+    let reverseEl = "";
+
+    for (let i = val.length - 1; i >= 0; i--) {
+        reverseEl += val[i];
+    }
+
+    console.log(val === reverseEl);
+    
+}
+
+isPalindrome("racecar");
